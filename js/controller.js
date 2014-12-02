eb9982d4aa3b7b66aa4d2670cf8dd3ba04f63c34
bogdan12893 <bogdan1281993@gmail.com>
2014-12-02 23:12:31 +0200

    update JS

---
+++ js/controller.js
@@ -0,0 +1,6 @@
+document.getElementById("nav01").innerHTML =
+"<ul>" +
+"<li><a class="tab" href="index.html">Home</a></li>" +
+"<li><a class="tab" href="about.html">About</a></li>" +
+"<li><a class="tab" href="contact.html">Contact</a></li>" +
+"</ul>";
